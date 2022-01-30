b8f29585a87b61d03ba0ff839d85f0f0427353ac
Daniel Friesen <d@danf.ca>
2022-01-29 16:35:46 -0800

    Polish the design of the code blocks a bit

--- src/ui/CodeFile.tsx
+++ src/ui/CodeFile.tsx
@@ -1,6 +1,12 @@
 import '../syntax-highlighter/lighttpd';
 import { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
 import sunburst from 'react-syntax-highlighter/dist/esm/styles/hljs/sunburst';
+import { styled } from '@material-ui/core';
+
+const PreTag = styled('pre', { name: 'CodeFile', slot: 'PreTag' })(
+  ({ theme }) => ({ borderRadius: theme.shape.borderRadius })
+);
+const CodeTag = styled('code', { name: 'CodeFile', slot: 'CodeTag' })();
 
 export interface CodeFileProps {
   type: string;
@@ -14,8 +20,14 @@ export const CodeFile = (props: CodeFileProps) => {
   const { type, content } = props;
 
   return (
-    <SyntaxHighlighter language={type} style={sunburst} wrapLongLines>
-      {content}
+    <SyntaxHighlighter
+      language={type}
+      style={sunburst}
+      wrapLongLines
+      PreTag={PreTag}
+      CodeTag={CodeTag}
+    >
+      {content.trimEnd()}
     </SyntaxHighlighter>
   );
 };
